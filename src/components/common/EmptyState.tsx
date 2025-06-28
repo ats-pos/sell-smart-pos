58f27fea5a828ddc6246ed5e3a866076454bec5d
Selva <selvaprvn@gmail.com>
2025-06-28 15:38:50 +0530

    Updated EmptyState.tsx

--- src/components/common/EmptyState.tsx
+++ src/components/common/EmptyState.tsx
@@ -1,6 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
-import { LucideIcon } from "lucide-react";
+import { DivideIcon as LucideIcon } from "lucide-react";
 
 interface EmptyStateProps {
   icon: LucideIcon;
